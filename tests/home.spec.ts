34af454f30a144659c8bc3221a10629c0f69630a
Nataliia Hromenko <gromenkowork@gmail.com>
2023-03-22 22:32:38 +0000

    create const for allowCookiesButton

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,7 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Visit Website', async ({ page }) => {
-    await page.goto('https://test-english.com/');
-    await expect(page).toHaveTitle(/Test your English/);
-    await page.getByRole('button', { name: 'Allow all cookies' }).click();
-    });
+test("Visit Website and Accept Cookies", async ({ page }) => {
+  await page.goto('https://test-english.com/');
+  await expect(page).toHaveTitle(/Test your English/);
+  const allowCookiesButton = page.getByRole("button", {
+    name: "Allow all cookies",
+  });
+  await allowCookiesButton.click();
+});
